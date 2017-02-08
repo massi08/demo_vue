7ddb0e68ceaa27ff571d3c1cd78f018870c94d09
Massi <m.bouneffa@upfit-services.com>
2017-02-08 15:17:30 +0100

    Add NotFound Page

---
+++ src/components/notFound/NotFound.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="not-found">
+    <h1 class="md-display-4">
+      <md-icon class="md-size-4x">sentiment_very_dissatisfied</md-icon>
+      404 Not Found
+      <md-icon class="md-size-4x">sentiment_very_dissatisfied</md-icon>
+    </h1>
+    <router-link to="/" tag="div" class="link-wrap">
+      <md-button class="md-raised md-primary">
+        <md-icon>home</md-icon>
+        Retournez vers la page d'acceuil
+      </md-button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'not-found'
+    }
+</script>
+
+<style lang="scss" scoped>
+  .not-found {
+    border: 1px solid transparent !important; /*Margin collapse*/
+    top: 50%;
+    transform: translate(0, -50%);
+    position: absolute;
+    width: 100%;
+  }
+
+  .link-wrap {
+    text-align: center;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from 'components/home/Home'
 import About from 'components/about/About'
+import NotFound from 'components/notFound/NotFound'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
             path:'/about',
             name: 'About',
             component: About
+        },
+        {
+            path:'*',
+            name: 'NotFound',
+            component: NotFound
         }
     ]
 })
