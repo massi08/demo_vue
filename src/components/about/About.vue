155fb03937592d1b22d0a50743621a2bd4d9d875
Massi <m.bouneffa@upfit-services.com>
2017-02-09 02:30:22 +0100

    Todo complete About

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -3,8 +3,17 @@
     <my-header></my-header>
     <div class="main-content">
       <h1>About</h1>
+      <md-whiteframe elevation="2" class="test-model-wrapper">
+        <div>
+          <md-layout md-align="center">
+            <p>A little paragraph about oursleves</p>
+          </md-layout>
+          <md-layout md-align="center">
+            Links we used
+          </md-layout>
+        </div>
+      </md-whiteframe>
     </div>
-  </div>
 </template>
 
 <script>
